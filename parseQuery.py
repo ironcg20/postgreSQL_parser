9f34d7c151113c95ecacb2834719ab530c1ca49a
ironcg20 <149658174+ironcg20@users.noreply.github.com>
2023-12-27 01:01:43 -0600

    query_id: extracted

--- parseQuery.py
+++ parseQuery.py
@@ -1,14 +1,37 @@
 import re
 
+
 def parseQuery(query):
     query_id_pattern = r'query_id: (\S+)'
-    query_id_match = re.search(query_id_pattern, query)
+    sql_text_pattern = r'sql_text: (.+?)query_type:'
+    database_name_pattern = r'database_name: (\w+)'
+    schema_name_pattern = r'schema_name: (\w+)'
 
-    if query_id_match and sql_text_match:
-    print(query_id_pattern)
+    query_id_match = re.search(query_id_pattern, query)
+    sql_text_match = re.search(sql_text_pattern, query, re.DOTALL)
+    database_name_match = re.search(database_name_pattern, query)
+    schema_name_match = re.search(schema_name_pattern, query)
+
+    if query_id_match:
+        query_id = query_id_match.group(1)
+        # print(f"Query ID: {query_id}")
+
+    if sql_text_match:
+        sql_text = sql_text_match.group(1).strip()
+        print(f"SQL Text:\n{sql_text}")
+    else:
+        print("SQL text not found.")
+
+    if database_name_match:
+        database_name = database_name_match.group(1)
+        # print(f"Database Name: {database_name}")
+
+    if schema_name_match:
+        schema_name = schema_name_match.group(1)
+        # print(f"Schema Name: {schema_name}")
 
 
 if __name__ == "__main__":
     with open("query.txt", 'r') as file:
-        query=file.read()
+        query = file.read()
     parseQuery(query)
